28488d79d4912d9c1011756f375377d55ad39cf1
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2022-12-23 18:27:41 +0900

    save

--- apps/app-one/pages/index.tsx
+++ apps/app-one/pages/index.tsx
@@ -4,7 +4,7 @@ import { Button } from 'ui';
 const Home: NextPage = () => {
   return (
     <div>
-      <h2>monorepoTemplate</h2>
+      <h2>monorepo Template</h2>
       <Button />
     </div>
   );
