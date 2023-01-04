c539c63ec33b23c553a7175bd4b2f11a91a4e56e
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2023-01-04 16:40:16 +0900

    fix code

--- apps/app-one/next.config.js
+++ apps/app-one/next.config.js
@@ -5,6 +5,7 @@ const path = require('path');
 module.exports = withTM({
   sassOptions: {
     includePaths: [path.resolve(__dirname, '../../packages/common/styles/')],
+    additionalData: `@use '../../packages/common/styles/variables.scss' as *;`,
   },
   reactStrictMode: true,
 });
--- apps/app-two/next.config.js
+++ apps/app-two/next.config.js
@@ -4,7 +4,8 @@ const path = require('path');
 
 module.exports = withTM({
   sassOptions: {
-    includePaths: [path.join(__dirname, '../../packages/common/styles')],
+    includePaths: [path.resolve(__dirname, '../../packages/common/styles/')],
+    additionalData: `@use '../../packages/common/styles/variables.scss' as *;`,
   },
   reactStrictMode: true,
 });
